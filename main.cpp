a67d3c282c47177cad75e1a67fa1be76a97a2652
mr.Escow <escowxbox@gmail.com>
2021-05-16 19:32:15 +0300

    2

--- main.cpp
+++ main.cpp
@@ -2,9 +2,33 @@
 
 using std::cout;
 using std::endl;
+using std::cin;
+
+// Написать функцию проверки, является ли число простым
+
+bool is_simple(uint16_t number) {
+	if (number == 1) return false;
+	for (int d = 2; d * d <= number; d++) {
+		if (number % d == 0) 
+			return false;
+	}
+	return true;
+}
 
 int main() {
-	cout << "Hello World!" << endl;
+
+	uint16_t number;
+	do {
+		cout << "\nInput 0 for exit.\nInput number :  ";
+		cin >> number;
+		if (number == 0) break;
+		if(is_simple(number))
+			cout << "Number   " << number << "  prostoe!\n";
+		else
+			cout << "Number   " << number << "  ne prostoe!\n";
+	} while (number != 0);
+	
+
 	system("pause");
 	return 0;
 }
